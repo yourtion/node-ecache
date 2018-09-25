b970abfefea4568637628ada4fc6dd331cc7e4b6
YourtionGuo <yourtion@gmail.com>
2018-09-25 12:25:47 +0800

    refactor(memory): mv getData and setData to Store

--- src/memory.ts
+++ src/memory.ts
@@ -22,13 +22,6 @@ export default class MemoryStore<T = any> extends Store {
   private cache: Record<string, ICacheItem<T>> = Object.create(null);
   /** 是否不可变 */
   private immutable: boolean;
-  /** 默认缓存时间 */
-  private ttl: number;
-
-  /** 方法队列 */
-  private fnQueue: Record<string, Promise<T | undefined>> = Object.create(null);
-  /** 获取数据方法 */
-  private fns: Record<string, IFnType<T>> = Object.create(null);
 
   constructor({ ttl = 30, immutable = true }: IMemOption = {}) {
     super();
@@ -70,46 +63,5 @@ export default class MemoryStore<T = any> extends Store {
    */
   delete(key: string) {
     delete this.cache[key];
-  }
-
-  /**
-   * 注册通过 fn 获取 key 数据方法
-   * @param key 获取数据Key
-   * @param fn 获取数据方法
-   */
-  setData(key: string, fn: IFnType<T>) {
-    this.fns[key] = fn;
-  }
-
-  /**
-   * 通过注册的 fn 获取数据并缓存
-   * @param key 获取数据Key
-   * @param args 获取数据参数
-   */
-  getData(key: string, ...args: any[]) {
-    const fn = this.fns[key];
-    if (!fn) throw new Error(key + " is not setData yet");
-    const cacheKey = args.length > 0 ? key + JSON.stringify(args) : key;
-    if (!this.fnQueue[cacheKey]) {
-      this.fnQueue[cacheKey] = this.get(cacheKey)
-        .then(data => {
-          if (data !== undefined) {
-            delete this.fnQueue[cacheKey];
-            return data;
-          }
-          return fn(...args);
-        })
-        .then(res => {
-          if (!this.fnQueue[cacheKey]) return res;
-          return this.set(cacheKey, res);
-        })
-        .then(res2 => {
-          if (!this.fnQueue[cacheKey]) return res2;
-          delete this.fnQueue[cacheKey];
-          if (!res2) return undefined;
-          return this.get(cacheKey);
-        });
-    }
-    return this.fnQueue[cacheKey];
   }
 }
--- src/store.ts
+++ src/store.ts
@@ -2,14 +2,57 @@ export type IFnType<T> = (...args: any[]) => Promise<T>;
 
 /** 存储引擎 */
 export default abstract class Store<T = any> {
+  /** 默认缓存时间 */
+  protected ttl: number = 0;
+  /** 方法队列 */
+  private fnQueue: Record<string, Promise<T | undefined>> = Object.create(null);
+  /** 获取数据方法 */
+  private fns: Record<string, IFnType<T>> = Object.create(null);
+
   /** 获取数据 */
   abstract get(key: string): Promise<T | undefined>;
   /** 设置数据 */
   abstract set(key: string, data: T, ttl: number): Promise<T>;
   /** 删除数据 */
   abstract delete(key: string): void;
-  /** 设置数据源获取函数 */
-  abstract setData(key: string, fn: IFnType<T>): void;
-  /** 通过缓存或数据源获取数据 */
-  abstract getData(key: string, ...args: any[]): Promise<T | undefined>;
+  /**
+   * 注册通过 fn 获取 key 数据方法
+   * @param key 获取数据Key
+   * @param fn 获取数据方法
+   */
+  setData(key: string, fn: IFnType<T>) {
+    this.fns[key] = fn;
+  }
+
+  /**
+   * 通过注册的 fn 获取数据并缓存
+   * @param key 获取数据Key
+   * @param args 获取数据参数
+   */
+  getData(key: string, ...args: any[]) {
+    const fn = this.fns[key];
+    if (!fn) throw new Error(key + " is not setData yet");
+    const cacheKey = args.length > 0 ? key + JSON.stringify(args) : key;
+    if (!this.fnQueue[cacheKey]) {
+      this.fnQueue[cacheKey] = this.get(cacheKey)
+        .then(data => {
+          if (data !== undefined) {
+            delete this.fnQueue[cacheKey];
+            return data;
+          }
+          return fn(...args);
+        })
+        .then(res => {
+          if (!this.fnQueue[cacheKey]) return res;
+          return this.set(cacheKey, res, this.ttl);
+        })
+        .then(res2 => {
+          if (!this.fnQueue[cacheKey]) return res2;
+          delete this.fnQueue[cacheKey];
+          if (!res2) return undefined;
+          return this.get(cacheKey);
+        });
+    }
+    return this.fnQueue[cacheKey];
+  }
 }
