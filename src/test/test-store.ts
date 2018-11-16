f2ffabd384df9408a830fb9a7e06d87214bb78e3
YourtionGuo <yourtion@gmail.com>
2018-11-16 22:45:41 +0800

    test: update test

--- src/test/test-store.ts
+++ src/test/test-store.ts
@@ -4,27 +4,23 @@ const KEY = "a";
 const VALUES = [{ a: 1, b: { b: 1 } }, "Hello Yourtion", 12.02, null];
 const VAL_OBJ = VALUES[0];
 
-const sleep = (time: number) => {
-  return new Promise(resolve => {
-    return setTimeout(resolve, time);
-  });
-};
+const sleep = (time: number) => new Promise(resolve => setTimeout(resolve, time));
 
 describe("Libs - MemoryStore immutable true", () => {
   const cache = new MemoryStore({ ttl: 0.01 });
 
   it("Test - simple get set delete", async function() {
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
     for (const val of VALUES) {
       await cache.set(KEY, val);
-      expect(await cache.get(KEY)).toEqual(val);
+      expect(cache.get(KEY)).resolves.toEqual(val);
       cache.delete(KEY);
-      expect(await cache.get(KEY)).toBeUndefined();
+      expect(cache.get(KEY)).resolves.toBeUndefined();
     }
   });
 
   it("Test - immutable object", async function() {
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
     cache.set(KEY, VAL_OBJ);
     const res = await cache.get(KEY);
     expect(res).toEqual(VAL_OBJ);
@@ -34,7 +30,7 @@ describe("Libs - MemoryStore immutable true", () => {
   it("Test - cache expire", async function() {
     cache.set(KEY, VAL_OBJ);
     await sleep(10);
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
   });
 });
 
@@ -42,28 +38,28 @@ describe("Libs - MemoryStore immutable false", () => {
   const cache = new MemoryStore({ immutable: false, ttl: 0.01 });
 
   it("Test - simple get set delete", async function() {
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
     for (const val of VALUES) {
       cache.set(KEY, val);
-      expect(await cache.get(KEY)).toEqual(val);
+      expect(cache.get(KEY)).resolves.toEqual(val);
       cache.delete(KEY);
-      expect(await cache.get(KEY)).toBeUndefined();
+      expect(cache.get(KEY)).resolves.toBeUndefined();
     }
   });
 
   it("Test - mutable object", async function() {
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
     cache.set(KEY, VAL_OBJ);
     const res = await cache.get(KEY);
     expect(res).toEqual(VAL_OBJ);
     res.a = 2;
     expect(res.a).toEqual(2);
-    expect(await cache.get(KEY)).toEqual(VAL_OBJ);
+    expect(cache.get(KEY)).resolves.toEqual(VAL_OBJ);
   });
 
   it("Test - cache expire", async function() {
     await cache.set(KEY, VAL_OBJ);
     await sleep(10);
-    expect(await cache.get(KEY)).toBeUndefined();
+    expect(cache.get(KEY)).resolves.toBeUndefined();
   });
 });
